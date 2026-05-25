39c4bb34767f0e3e0f1c0cf8347b62dcca88b472
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2026-05-25 15:26:15 +0300

    feat(applet): add SHA3-256 HASH instruction
      - INS=0x20 hashes input bytes with SHA3-256 and returns 32-byte digest
      - MessageDigest allocated once in constructor, reused per-call
      - Output written to transient byte array (CLEAR_ON_DESELECT)
      - Tests verified against NIST FIPS 202 known-answer test vectors
        for empty input and 'abc'

--- applet/src/main/java/com/javacard/pqc/applet/PqcApplet.java
+++ applet/src/main/java/com/javacard/pqc/applet/PqcApplet.java
@@ -1,14 +1,26 @@
 package com.javacard.pqc.applet;
 
+import javacard.security.MessageDigest;
+
 import javacard.framework.*;
 
 public class PqcApplet extends Applet {
 
     public static final byte[] AID_BYTES = { (byte) 0xF0, 0x00, 0x00, 0x00, 0x01 };
 
+    private MessageDigest md;
+
+    private byte[] buffer;
+
+    private final static short BUFFER_SIZE = 32;
+
     public final static int INS_ECHO = 0x10;
 
+    public final static int INS_HASH = 0x20;
+
     private PqcApplet() {
+        md = MessageDigest.getInstance(MessageDigest.ALG_SHA3_256, false);
+        buffer = JCSystem.makeTransientByteArray(BUFFER_SIZE, JCSystem.CLEAR_ON_DESELECT);
     }
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {
@@ -25,6 +37,10 @@ public class PqcApplet extends Applet {
                 echo(apdu);
                 break;
 
+            case INS_HASH:
+                hash(apdu);
+                break;
+
             default:
                 ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
                 break;
@@ -36,5 +52,13 @@ public class PqcApplet extends Applet {
         apdu.setOutgoing();
         apdu.setOutgoingLength(len);
         apdu.sendBytes(ISO7816.OFFSET_CDATA, len);
+    }
+
+    private void hash(APDU apdu) {
+        short len = apdu.setIncomingAndReceive();
+        md.doFinal(apdu.getBuffer(), ISO7816.OFFSET_CDATA, len, buffer, (short) 0);
+        apdu.setOutgoing();
+        apdu.setOutgoingLength(BUFFER_SIZE);
+        apdu.sendBytesLong(buffer, (short) 0, BUFFER_SIZE);
     }
 }
--- applet/src/test/java/com/javacard/pqc/applet/PqcAppletTest.java
+++ applet/src/test/java/com/javacard/pqc/applet/PqcAppletTest.java
@@ -14,9 +14,13 @@ class PqcAppletTest {
 
     private CardSimulator simulator;
 
-    public final static int CLA = 0x00;
+    private final static int CLA = 0x00;
 
-    public final static int INS_SELECT = 0xA4;
+    private final static int INS_SELECT = 0xA4;
+
+    private final static String EMPTY_SHA3_256_OUTPUT = "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a";
+
+    private final static String ABC_SHA3_256_OUTPUT = "3a985da74fe225b2045c172d6bd390bd855f086e3e9d525b46bfe24511431532";
 
     private CardSimulator buildSimulator() {
         CardSimulator simulator = new CardSimulator();
@@ -30,6 +34,15 @@ class PqcAppletTest {
         simulator = buildSimulator();
         simulator
                 .transmitCommand(new CommandAPDU(CLA, INS_SELECT, 0x04, 0x00, PqcApplet.AID_BYTES));
+    }
+
+    private byte[] hexToBytes(String hex) {
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < hex.length(); i += 2) {
+            bytes[i / 2] = (byte) Integer.parseInt(hex.substring(i, i + 2), 16);
+        }
+
+        return bytes;
     }
 
     @Test
@@ -54,5 +67,22 @@ class PqcAppletTest {
         ResponseAPDU response = simulator.transmitCommand(echo);
         assertEquals(0x9000, response.getSW());
         assertArrayEquals(payload, response.getData());
+    }
+
+    @Test
+    void hashOfEmptyInputReturnSuccessAndMatchesNistVector() {
+        CommandAPDU emptyHash = new CommandAPDU(CLA, PqcApplet.INS_HASH, 0x00, 0x00);
+        ResponseAPDU response = simulator.transmitCommand(emptyHash);
+        assertEquals(0x9000, response.getSW());
+        assertArrayEquals(hexToBytes(EMPTY_SHA3_256_OUTPUT), response.getData());
+    }
+
+    @Test
+    void hashOfAbcReturnSuccessAndMatchesNistVector() {
+        byte[] payload = { 0x61, 0x62, 0x63 };
+        CommandAPDU abcHash = new CommandAPDU(CLA, PqcApplet.INS_HASH, 0x00, 0x00, payload);
+        ResponseAPDU response = simulator.transmitCommand(abcHash);
+        assertEquals(0x9000, response.getSW());
+        assertArrayEquals(hexToBytes(ABC_SHA3_256_OUTPUT), response.getData());
     }
 }
